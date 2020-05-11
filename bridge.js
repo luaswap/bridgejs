670c3f55633aa5bf52f9f76dba6b3f2a8fddc486
pqv199x <pqv199x@hotmail.com>
2020-05-11 17:10:54 +0700

    add get wrap/unwrap tx functions, unwrap func

--- bridge.js
+++ bridge.js
@@ -6,9 +6,19 @@ const BigNumber = require('bignumber.js')
 const WebSocket = require('ws')
 const events = require('events')
 
-const validatorAddress = '0x0000000000000000000000000000000000000088'
+const WrapperAbi = require('./abis/WrapperAbi.json')
 
 let network = {}
+const sleep = m => new Promise(res => setTimeout(res, m))
+
+function string2bytes (str) {
+    let byteArray = []
+    for (let j = 0; j < str.length; j++) {
+        byteArray.push(str.charCodeAt(j))
+    }
+
+    return byteArray
+}
 
 /**
  * The SDK works with TomoChain protocols
@@ -35,6 +45,13 @@ class BridgeJS {
             this.provider = new ethers.providers.JsonRpcProvider(rpcEndpoint)
         }
 
+        if (config.swapCoin) {
+            const coin = {}
+            config.swapCoin.forEach(c => {
+                coin[c.name.toLowerCase()] = c
+            })
+            this.swapCoin = coin
+        }
         this.wallet = new ethers.Wallet(pkey, this.provider)
         this.coinbase = this.wallet.address
 
@@ -150,6 +167,36 @@ class BridgeJS {
         })
     }
 
+    async getWithdrawTransaction ({ tokenSymbol }) {
+        return new Promise((resolve, reject) => {
+            try {
+                let url = urljoin(this.endpoint, 'api/wrap/getTransaction/withdraw/', tokenSymbol, this.coinbase)
+                let options = {
+                    method: 'GET',
+                    url: url,
+                    json: true,
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: {}
+                }
+                request(options, (error, response, body) => {
+                    if (error) {
+                        return reject(error)
+                    }
+                    if (response.statusCode !== 200 && response.statusCode !== 201) {
+                        return reject(body)
+                    }
+
+                    return resolve(body)
+
+                })
+            } catch(e) {
+                return reject(e)
+            }
+        })
+    }
+
     async wrapWatch ({ tokenSymbol }) {
         return new Promise((resolve, reject) => {
             const ev = new events.EventEmitter()
@@ -183,17 +230,194 @@ class BridgeJS {
             ev.emit('open')
 
         })
+    }
 
+    async unwrapWatch ({ tokenSymbol }) {
+        return new Promise((resolve, reject) => {
+            const ev = new events.EventEmitter()
+            let tomojs = this
+
+            function wait () {
+                let it = setInterval(async () => {
+                    try {
+                        let data = await tomojs.getWithdrawTransaction({ tokenSymbol })
+                        if (data && data.transaction) {
+                            ev.emit('message', data.transaction)
+                        }
+                    } catch(e) {
+                        clearInterval(it)
+                        ev.emit('close')
+                    }
+                }, 2000)
+            }
+
+            ev.addListener('wait', wait)
+
+            ev.on('close', () => { 
+                resolve()
+            })
+
+            ev.on('open', () => {
+                ev.emit('wait')
+                resolve(ev)
+            })
+
+            ev.emit('open')
+
+        })
+    }
+
+    async getWrapTransactions ({ limit, page }) {
+        return new Promise((resolve, reject) => {
+            try {
+                let url = urljoin(
+                    this.endpoint, 'api/transactions/getWrapTxs/',
+                    `?address=${this.coinbase}`,
+                    `&page=${page}`,
+                    `&limit=${limit}`
+                )
+                let options = {
+                    method: 'GET',
+                    url: url,
+                    json: true,
+                    headers: {
+                        'Accept': 'application/json',
+                        'content-type': 'application/json'
+                    },
+                    body: {}
+                }
+                request(options, (error, response, body) => {
+                    if (error) {
+                        return reject(error)
+                    }
+                    if (response.statusCode !== 200 && response.statusCode !== 201) {
+                        return reject(body)
+                    }
+
+                    return resolve(JSON.stringify(body, null, 2))
+
+                })
+            } catch(e) {
+                return reject(e)
+            }
+        })
+    }
+
+    async getUnwrapTransactions ({ tokenSymbol, limit, page }) {
+        return new Promise((resolve, reject) => {
+            try {
+                let url = urljoin(
+                    this.endpoint, 'api/transactions/getUnwrapTxs/',
+                    `?address=${this.coinbase}`,
+                    `&page=${page}`,
+                    `&limit=${limit}`
+                )
+                let options = {
+                    method: 'GET',
+                    url: url,
+                    json: true,
+                    headers: {
+                        'Accept': 'application/json',
+                        'content-type': 'application/json'
+                    },
+                    body: {}
+                }
+                request(options, (error, response, body) => {
+                    if (error) {
+                        return reject(error)
+                    }
+                    if (response.statusCode !== 200 && response.statusCode !== 201) {
+                        return reject(body)
+                    }
+
+                    return resolve(JSON.stringify(body, null, 2))
+
+                })
+            } catch(e) {
+                return reject(e)
+            }
+        })
+    }
+
+    async getTransactions ({ tokenSymbol, limit, page }) {
+        return new Promise((resolve, reject) => {
+            try {
+                let url = urljoin(
+                    this.endpoint, 'api/transactions/',
+                    `?coin=${tokenSymbol}`,
+                    `&page=${page}`,
+                    `&limit=${limit}`
+                )
+                let options = {
+                    method: 'GET',
+                    url: url,
+                    json: true,
+                    headers: {
+                        'Accept': 'application/json',
+                        'content-type': 'application/json'
+                    },
+                    body: {}
+                }
+                request(options, (error, response, body) => {
+                    if (error) {
+                        return reject(error)
+                    }
+                    if (response.statusCode !== 200 && response.statusCode !== 201) {
+                        return reject(body)
+                    }
+
+                    return resolve(JSON.stringify(body, null, 2))
+
+                })
+            } catch(e) {
+                return reject(e)
+            }
+        })
     }
 
     /**
-	 * Unwrap TRC20 TOKEN
+	 * Unwrap TRC21 TOKEN
 	 * @param {object} unwrap - The unwrap information
-	 * @param {string} unwrap.tokenAddress - The token address
+	 * @param {string} unwrap.tokenSymbol - The token symbol
 	 * @param {string} unwrap.amount - The amount for unwrapping
 	 * @param {string} unwrap.dest - The destination address
 	 */
-    async unwrap ({ tokenAddress, amount, dest }) { }
+    async unwrap ({ tokenSymbol, amount, dest }) {
+        try {
+            const coin = this.swapCoin[tokenSymbol.toLowerCase()]
+            const contract = new ethers.Contract(
+                coin.wrapperAddress,
+                WrapperAbi.abi,
+                this.wallet
+            )
+            let txParams = {
+                gasPrice: await this.provider.getGasPrice(),
+                gasLimit: ethers.utils.hexlify(2000000),
+                nonce: await this.provider.getTransactionCount(this.coinbase)
+            }
+            const withdrawAmountBN = new BigNumber(amount).multipliedBy(10 ** coin.decimals).toString(10)
+            const result = await contract.functions.burn(
+                withdrawAmountBN.toString(10),
+                string2bytes(dest),
+                txParams
+            )
+            let check = true, receipt
+            console.log('Verify tx...')
+            await sleep(5000)
+            while (check) {
+                receipt = await this.provider.getTransactionReceipt(result.hash || '')
+                if (receipt.status) {
+                    check = false
+                } else {
+                    check = false
+                    throw new Error('Something went wrong \n txHash: ' + result.hash || '')
+                }
+            }
+            return receipt
+        } catch (error) {
+            throw error    
+        }
+    }
 
 }
 
